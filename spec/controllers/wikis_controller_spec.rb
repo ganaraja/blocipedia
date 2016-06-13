1e4b017390792416105d0081a2c20143206db957
Ganaraja <ganaraja@gmail.com>
2016-06-13 06:23:03 +0530

    Checkpoint 4 - new

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -23,6 +23,23 @@ RSpec.describe WikisController, type: :controller do
     end
   end
 
+   describe "GET new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
+     end
+
+     it "instantiates @wiki" do
+       get :new
+       expect(assigns(:wiki)).not_to be_nil
+     end
+   end
+
   # describe "GET #show" do
   #   it "returns http success" do
   #     get :show
