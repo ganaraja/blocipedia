b6c57586c9454dd32f834c518dc125f9c344c03b
Ganaraja <ganaraja@gmail.com>
2016-06-27 22:15:14 +0530

    Checkpoint 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
-require 'random_data'
-
+require 'faker'
 
 50.times do
+user =  User.new(
+    email: Faker::Internet.email,
+    password:Faker::Internet.password
+  )
+  user.skip_confirmation!
+  user.save
+end
 
+users = User.all
+
+100.times do
   Wiki.create!(
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+    title: Faker::Name.name,
+    body: Faker::Lorem.paragraph,
+    user: users.sample
   )
 end
+
 wikis = Wiki.all
 
-puts "Seed finished"
+puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Wiki.count} wikis created"
